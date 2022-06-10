f52784a41d13011f0e95b3ab8d949e9ff4a557e7
Chandrakant <chandrakantsavvy@gmail.com>
2022-06-10 11:43:19 +0530

    sorting: bubble sort

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,17 @@
+package Sorting;
+
+public class BubbleSort {
+    public int[] sort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/Sorting/BubbleSortTest.java
@@ -0,0 +1,17 @@
+package Sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    void testBubbleSort() {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] input = { 1, -2, 4, 8, 5, 3, 9};
+        int[] expected = {-2, 1, 3, 4, 5, 8, 9};
+        int[] actual = bubbleSort.sort(input);
+        assertArrayEquals(expected, actual);
+    }
+}
